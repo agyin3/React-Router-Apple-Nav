743be88cb06433939ce2027a45c2f926bc09dd8c
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-10-08 20:59:05 -0600

    add mock data

---
+++ apple-nav/src/data.js
@@ -0,0 +1,202 @@
+
+const menu = [
+    {
+        index: 0,
+        title: 'Mac',
+        subMenu: [
+            {
+                name: 'Macbook Air'
+            },
+            {
+                name: 'Macbook Pro'
+            },
+            {
+                name: 'iMac'
+            },
+            {
+                name: 'iMac Pro'
+            },
+            {
+                name: 'Mac Pro'
+            },
+            {
+                name: 'Mac Mini'
+            },
+            {
+                name: 'Compare'
+            },
+            {
+                name: 'Pro Display XDR'
+            },
+            {
+                name: 'Accessories'
+            },
+            {
+                name: 'Catalina'
+            }
+        ]
+    },
+
+    {
+        index: 1,
+        title: 'iPad',
+        subMenu: [
+            {
+                name: 'iPad Pro'
+            },
+            {
+                name: 'iPad Air'
+            },
+            {
+                name: 'iPad'
+            },
+            {
+                name: 'iPad Mini'
+            },
+            {
+                name: 'Compare'
+            },
+            {
+                name: 'Apple Pencil'
+            },
+            {
+                name: 'Smart Keyboard'
+            },
+            {
+                name: 'Accessories'
+            },
+            {
+                name: 'iPadOS'
+            }
+        ]
+    },
+
+    {
+        index: 2,
+        title: 'iPhone',
+        subMenu: [
+            {
+                name: 'iPhone 11 Pro'
+            },
+            {
+                name: 'iPhone 11'
+            },
+            {
+                name: 'iPhone XR'
+            },
+            {
+                name: 'iPhone 8'
+            },
+            {
+                name: 'Compare'
+            },
+            {
+                name: 'Apple Card'
+            },
+            {
+                name: 'Airpods'
+            },
+            {
+                name: 'Accessories'
+            },
+            {
+                name: 'iOS 13'
+            }
+        ]
+    },
+
+    {
+        index: 3,
+        title: 'Watch',
+        subMenu: [
+            {
+                name: 'Apple Watch Series 5'
+            },
+            {
+                name: 'Apple Watch Nike'
+            },
+            {
+                name: 'Apple Watch Hermes'
+            },
+            {
+                name: 'Apple Watch Edition'
+            },
+            {
+                name: 'Apple Watch Series 3'
+            },
+            {
+                name: 'Compare'
+            },
+            {
+                name: 'Bands'
+            },
+            {
+                name: 'Airpods'
+            },
+            {
+                name: 'Accessories'
+            },
+            {
+                name: 'watchOS'
+            }
+        ]
+    },
+
+    {
+        index: 4,
+        title: 'TV',
+        subMenu: [
+            {
+                name: 'Apple TV app'
+            },
+            {
+                name: 'Apple TV+'
+            },
+            {
+                name: 'Apple TV 4k'
+            },
+            {
+                name: 'Apple TV HD'
+            },
+            {
+                name: 'Airplay'
+            },
+            {
+                name: 'Accessories'
+            }
+        ]
+    },
+
+    {
+        index: 5,
+        title: 'Music',
+        subMenu: [
+            {
+                name: 'Apple Music'
+            },
+            {
+                name: 'HomePod'
+            },
+            {
+                name: 'AirPods'
+            },
+            {
+                name: 'iPod Touch'
+            },
+            {
+                name: 'Music Accessoires'
+            },
+            {
+                name: 'Gift Cards'
+            }
+        ]
+    },
+
+    {
+        index: 6,
+        title: 'Support',
+        subMenu: []
+    }
+]
+
+export default menu;
